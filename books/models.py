1a6237d2c2e6864f8271f5157bd5811fed98cea1
sba22385 <sba22385@student.cct.ie>
2025-07-28 17:14:01 +0100

    Add files via upload

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
